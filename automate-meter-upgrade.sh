badfcb773f5a2b4b72ed4a61c0fe2de039c5d744
vindicory <ryan@vindico.net>
2024-01-14 20:44:02 +0000

    changed the order of the sequence

--- automate-meter-upgrade.sh
+++ automate-meter-upgrade.sh
@@ -1,6 +1,8 @@
+cd /home/pi/automate-node
+sudo sh automate-stop.sh
+cd /home/pi/automate-meter-upgrade
 sudo lcl-rpict-config.py -a -w node_meter.conf
 cd /home/pi/automate-node
 sudo sh automate-update.sh
-sudo sh automate-stop.sh
 sudo sh automate-start.sh
 lcl-rpict-config.py -a
