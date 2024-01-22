1dabcac60ea38b746e34bfb2acb497590d1fe0e3
vindicory <ryan@vindico.net>
2024-01-22 00:39:37 +0000

    added database backup

--- automate-meter-upgrade.sh
+++ automate-meter-upgrade.sh
@@ -1,7 +1,7 @@
 sudo sh /home/pi/automate-node/automate-stop.sh
 sudo cp /home/pi/automate-node/data/dump.rdb /home/pi/automate-node/backup_220120024.rdb
 sudo ls /home/pi/automate-node -l -h
-sudo /home/pi/automate-meter-upgrade/lcl-rpict-config.py -a -w node_meter.conf
+sudo /home/pi/automate-meter-upgrade/lcl-rpict-config.py -a -w /home/pi/automate-meter-upgrade/node_meter.conf
 sudo /home/pi/automate-meter-upgrade/lcl-rpict-config.py -a
 sudo sh /home/pi/automate-node/automate-update.sh
 sudo sh /home/pi/automate-node/automate-start.sh
