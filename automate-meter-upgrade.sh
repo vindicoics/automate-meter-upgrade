aeb637422fb6d8332be2fea33cab336aca267388
vindicory <ryan@vindico.net>
2024-01-22 00:55:54 +0000

    fix errors

--- automate-meter-upgrade.sh
+++ automate-meter-upgrade.sh
@@ -1,7 +1,9 @@
 sudo sh /home/pi/automate-node/automate-stop.sh
 sudo cp /home/pi/automate-node/data/dump.rdb /home/pi/automate-node/backup_220120024.rdb
 sudo ls /home/pi/automate-node -l -h
-sudo /home/pi/automate-meter-upgrade/lcl-rpict-config.py -a -w /home/pi/automate-meter-upgrade/node_meter.conf
-sudo /home/pi/automate-meter-upgrade/lcl-rpict-config.py -a
-sudo sh /home/pi/automate-node/automate-update.sh
-sudo sh /home/pi/automate-node/automate-start.sh
+sudo lcl-rpict-config.py -a -w /home/pi/automate-meter-upgrade/node_meter.conf
+sudo lcl-rpict-config.py -a
+cd /home/pi/automate-node/
+sudo sh automate-update.sh
+sudo sh automate-start.sh
+cd ..
